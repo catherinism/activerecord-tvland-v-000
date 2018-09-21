3cd37a765fd04c22e4f7bfd9fea7eb34912e00f5
Catherine Salazar <catcreates13@gmail.com>
2018-09-21 00:53:26 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -10,7 +10,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     self.characters.collect do |character|
-      "#{character.name} - #{self.show}"
+      "#{character.name} - "
     end
   end
 
