82cee31736d5dbf2d505e3744e19b5a21b7a931f
Catherine Salazar <catcreates13@gmail.com>
2018-09-21 01:13:42 +0000

    automatically backed up by learn

--- db/migrate/008_add_catchphrase_to_characters.rb
+++ db/migrate/008_add_catchphrase_to_characters.rb
@@ -1,6 +1,6 @@
 class AddCatchPraseToCharacters < ActiveRecord::Migration[5.1]
 
   def change
-    add_column :characters, :catchphrase, :string
+    add_column :characters, :catchphrase, :strings
   end
 end
